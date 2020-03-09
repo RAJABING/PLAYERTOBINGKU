d66f36ca0891ad62fa05b6f3bbbb1b922a28f0fb
RAJABING <59326452+RAJABING@users.noreply.github.com>
2020-03-09 16:37:22 +0700

    Add files via upload

---
+++ PLAYERTOBINGKU.sh
@@ -0,0 +1,22 @@
+clear
+password="BING"
+until [ "$siapa" = "$password" ]
+do
+  read -p "TULISKAN PASSWORLD NYA : " siapa
+done 
+
+
+echo $green "+.)MASUK KE TOOLS BINGKU"
+read -p  "pilihmana : " player
+
+if [ $player = + ] || [ $player = + ]
+then
+clear
+git clone https://github.com/RAJABING/bingku
+ls
+ls bingku
+cd bingku
+ls bingku
+sh bingku.sh
+fi 
+
